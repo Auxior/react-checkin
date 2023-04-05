d88a6c027233479f47bbf12f4f74dba65d432673
Mussued <skossm@outlook.com>
2023-04-05 14:13:08 +0800

    请求头发送token和获取用户信息

--- src/components/BeforeEach/BeforeEach.tsx
+++ src/components/BeforeEach/BeforeEach.tsx
@@ -1,19 +1,26 @@
 import React from "react";
 import { useLocation, matchRoutes, Navigate } from "react-router-dom";
 import { routes } from "../../router";
+import { useAppDispatch } from "../../store";
+import { infosAction } from "../../store/modules/users";
 
 interface BeforeEachProps {
   children?: React.ReactNode;
 }
 
 export default function BeforeEach(props: BeforeEachProps) {
+  const dispatch = useAppDispatch();
+
   const location = useLocation();
   const matchs = matchRoutes(routes, location);
   if (Array.isArray(matchs)) {
     const meta = matchs[matchs.length - 1].route.meta;
-    // if (meta?.auth) {
-    //   return <Navigate to="/login" />;
-    // }
+    if (meta?.auth) {
+      dispatch(infosAction()).then((action) => {
+        console.log(action.payload);
+      });
+      // return <Navigate to="/login" />;
+    }
   }
 
   return <>{props.children}</>;
